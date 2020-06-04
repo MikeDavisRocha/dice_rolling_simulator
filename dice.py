bc634e62975d5efa1ebf74782b1f11e4ea64a0dd
Mike Davis <mikedavisrocha@gmail.com>
2020-06-04 16:30:46 -0300

    Adding the input message

--- dice.py
+++ dice.py
@@ -3,4 +3,6 @@ from random import randint
 def roll_the_dice(num_of_faces):    
     return randint(1, num_of_faces)
 
-
+result = 0
+print("------ Rolling Dice Simulator ------\n"
+    "Please enter the number of faces from the dice: ")
