a36d1707274c72ec6e5c5ec6462b887d4648f480
Mike Davis <mikedavisrocha@gmail.com>
2020-06-04 16:31:41 -0300

    Adding the function call and result

--- dice.py
+++ dice.py
@@ -6,3 +6,6 @@ def roll_the_dice(num_of_faces):
 result = 0
 print("------ Rolling Dice Simulator ------\n"
     "Please enter the number of faces from the dice: ")
+number_of_faces = int(input())
+result = roll_the_dice(number_of_faces)
+print(f"The number sorted was: {result}")
